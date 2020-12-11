0a394f6ee90c724c709b8c357a422476d2977d4e
Harmen Stoppels <harmenstoppels@gmail.com>
2020-12-11 16:54:55 +0100

    more wip

--- multi_shift_cg_different_rhs.jl
+++ multi_shift_cg_different_rhs.jl
@@ -147,6 +147,91 @@ function multi_shift_cg_efficient!(x, A, b; maxiters=10, tol=1e-6)
     return x
 end
 
+function multi_shift_cg_efficient_shift!(X, A, B, shifts; maxiters=10, tol=1e-6)
+    n = size(A, 1)
+    m = length(shifts)
+
+    @assert size(X, 2) == size(B, 2) == m
+
+    # Start out with a zero guess for now.
+    fill!(X, 0)
+
+    # new search directions, unique for every shift.
+    u = zeros(n, m)
+
+    # The Krylov subspace basis vectors, probably there's some optimization left
+    # here to drop 1.
+    r = zeros(n)
+    t = zeros(n)
+    v = zeros(n)
+
+    # normalize the first krylov basis vec.
+    copyto!(r, view(B, :, 1))
+    β = norm(r)
+    r ./= β
+
+    # The running LU-decomposition, one lu-decomp per shift
+    LUᵢᵢ₋₁ = zeros(m)
+    h1 = 0.0
+    rhs = zeros(m)
+
+    residuals = [Float64[] for i = 1:m]
+
+    for iter = 1:maxiters
+        # new krylov basis vector
+        mul!(t, A, r)
+
+        # orthogonalize it to previous vecs
+        if iter > 1
+            t .-= h1 * v
+        end
+        
+        h2 = dot(r, t)
+        t .-= h2 .* r
+
+        # normalize
+        h3 = norm(t)
+        t ./= h3
+        
+        # do a step of LU decomp
+        LUᵢᵢ = h2 .+ shifts
+        if iter > 1
+            LUᵢᵢ .-= LUᵢᵢ₋₁ .* h1
+        end
+
+        # update the right-hand side
+        for k = 1:m
+            rhs[k] = (dot(r, view(B, :, k)) - h1 * rhs[k]) / LUᵢᵢ[k]
+        end
+
+        for k = 1:m
+            if iter > 1
+                u[:, k] .= r .- LUᵢᵢ₋₁[k] .* view(u, :, k) 
+            else
+                u[:, k] .= r
+            end
+        end
+
+        LUᵢᵢ₋₁ .= h3 ./ LUᵢᵢ
+
+        # axpy
+        for k = 1:m
+            X[:, k] .+= rhs[k] .* view(u, :, k)
+        end
+        
+        for k = 1:m
+            push!(residuals[k], norm(A * X[:, k] + shifts[k] .* X[:, k] .- B[:, k]))
+        end
+
+        # store stuff for the next iteration
+        copyto!(v, r)
+        copyto!(r, t)
+        h1 = h3
+    end
+
+    return X, residuals
+end
+
 function rand_spd(m, λs = range(1.0, m, length=m))
     F = qr!(rand(m, m))
     A = (F.Q * Diagonal(λs)) * F.Q'
@@ -155,21 +240,23 @@ end
 
 using IterativeSolvers
 
-function example(m = 100, maxiters=10)
-    A = rand_spd(m)
-    x = ones(m)
-    b = A * x
+function example(;m = 100, shifts = [0.0, 1.0, 2.0], maxiters=10)
+    n = length(shifts)
 
-    x_1 = cg(A, b, maxiter=maxiters)
-    x_2 = zeros(m)
-    multi_shift_cg_reference!(x_2, A, b, maxiters=maxiters)
+    A = rand_spd(m) + 10I
+    B = rand(m, n)
+    X = A \ B
 
-    x_3 = zeros(m)
-    multi_shift_cg_efficient!(x_3, A, b, maxiters=maxiters)
+    X_ref_cg = zeros(m, n)
 
-    @show norm(x_1 - x_2)
-    @show norm(x_2 - x_3)
+    for (i, σ) in enumerate(shifts)
+        X_ref_cg[:, i] .= cg(A + σ * I, B[:, i], maxiter=maxiters)
+    end
 
-    # return x_1, x_
-    return nothing
+    X_multi_cg = zeros(m, n)
+    X_multi_cg, resnorms = multi_shift_cg_efficient_shift!(X_multi_cg, A, B, shifts, maxiters=maxiters)
+
+    @show norm(X_ref_cg - X_multi_cg)
+
+    return resnorms, A, B, X, X_ref_cg, X_multi_cg
 end
