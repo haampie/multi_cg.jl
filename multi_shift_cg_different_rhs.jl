d79032f9a6fa3ca49a37b0dcdfd4bfb18a32c23f
Harmen Stoppels <harmenstoppels@gmail.com>
2020-12-11 15:52:40 +0100

    Add an efficient version

--- multi_shift_cg_different_rhs.jl
+++ multi_shift_cg_different_rhs.jl
@@ -73,6 +73,80 @@ function multi_shift_cg_reference!(x, A, b; maxiters=10, tol=1e-6)
     return x, A, V, H, LU
 end
 
+function multi_shift_cg_efficient!(x, A, b; maxiters=10, tol=1e-6)
+    n = size(A, 1)
+
+    fill!(x, 0)
+
+    u = zeros(n)
+    r = zeros(n)
+    t = zeros(n)
+    v = zeros(n)
+
+    copyto!(r, b)
+    β = norm(r)
+    r ./= β
+
+    rhs = β
+
+    h1 = 0.0
+    
+    LUᵢᵢ₋₁ = 0.0
+
+    for iter = 1:maxiters
+        # new krylov basis vector
+        mul!(t, A, r)
+
+        # orthogonalize it to previous vecs
+        if iter > 1
+            t .-= h1 .* v
+        end
+        
+        h2 = dot(r, t)
+        t .-= h2 .* r
+
+        # normalize
+        h3 = norm(t)
+        t ./= h3
+        
+        # do a step of LU decomp
+        LUᵢ₋₁ᵢ = h1
+        LUᵢᵢ = h2
+        if iter > 1
+            LUᵢᵢ -= LUᵢᵢ₋₁ * h1
+        end
+
+        LUᵢ₊₁ᵢ = h3 / LUᵢᵢ
+
+        # update the right-hand side
+        if iter > 1
+            rhs = -LUᵢ₋₁ᵢ * rhs
+        end
+        rhs /= LUᵢᵢ
+
+        # @show iter lu(H[1:iter,1:iter]).U' \ [β; zeros(iter-1)] - rhs[1:iter]
+
+        if iter > 1
+            u .= r .- LUᵢᵢ₋₁ .* u 
+        else
+            u .= r
+        end
+
+        # axpy
+        x .+= rhs .* u
+        
+        @show norm(A * x - b)
+
+        # store stuff for the next iteration
+        copyto!(v, r)
+        copyto!(r, t)
+        LUᵢᵢ₋₁ = LUᵢ₊₁ᵢ
+        h1 = h3
+    end
+
+    return x
+end
+
 function rand_spd(m, λs = range(1.0, m, length=m))
     F = qr!(rand(m, m))
     A = (F.Q * Diagonal(λs)) * F.Q'
@@ -86,13 +160,16 @@ function example(m = 100, maxiters=10)
     x = ones(m)
     b = A * x
 
-    @show size(A) size(x) size(b)
-
-    x_1, hist = cg(A, b, log=true, maxiter=maxiters)
-
+    x_1 = cg(A, b, maxiter=maxiters)
     x_2 = zeros(m)
     multi_shift_cg_reference!(x_2, A, b, maxiters=maxiters)
 
-    return x_1, x_2
+    x_3 = zeros(m)
+    multi_shift_cg_efficient!(x_3, A, b, maxiters=maxiters)
+
     @show norm(x_1 - x_2)
+    @show norm(x_2 - x_3)
+
+    # return x_1, x_
+    return nothing
 end
